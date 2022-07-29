36b472a8c9a3ae4385f0235b22ceca60c0147106
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-30 00:23:05 +0300

    start with the add users component

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container, Typography, FormControl, InputLabel, Input, Box, FormGroup, Button } from '@material-ui/core';
+import { addUser } from '../service/api';
+import { useHistory } from 'react-router-dom';
+
+const initialValue = {
+    name: "",
+    username : "",
+    email: "",
+    phone: "",
+} 
+
 
 const AddUser = () => {
   return (
