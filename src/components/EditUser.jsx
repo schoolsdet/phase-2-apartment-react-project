f43da17fddfdbeb9adb18563073c9d95f7c2f87d
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-30 01:22:42 +0300

    initial save of component

--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, FormControl, InputLabel, Input, Box, FormGroup, Button } from '@mui/material';
+import { editUser, getallUsers } from '../service/api';
+import { useHistory, useParams } from 'react-router-dom';
+
+const initialValue = { name: "", username : "", email: "", phone: ""}
 
 const EditUser = () => {
+  const [user, setUser] = useState(initialValue);
+  const {name, username, email, phone } = user;
+
+  const { id } - useParams();
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
+  const loadUserData = async () => {
+    const response = await getallUsers(id);
+    setUser(response.data);
+  }
+
+  const history = useHistory();
+
   return (
     <div>EditUser</div>
   )
