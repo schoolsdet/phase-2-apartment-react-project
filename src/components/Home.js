49276e7455e54ba575339aa4b239ad4e9cec812a
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-29 23:19:44 +0300

    home component done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {Container, Typography, Box } from '@material-ui/core'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+        <Box>
+            <Typography variant="h3" components="h2" align="center">Apartment Managent System</Typography>
+            <Typography component="h2" align="center">React Phase 2 Project</Typography>
+        </Box>
+    </Container>
   )
 }
 
