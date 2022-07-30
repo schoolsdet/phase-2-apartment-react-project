921711d89928dac698c598e9f3b4bc86678392de
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-30 08:26:00 +0300

    final commit

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,8 +3,7 @@ import notfound from './../assets/images/404page-website.png';
 
 const NotFound = () => {
   // const error = { textAlign: 'center',
-  //     marginTop: '20px', marginBottom: '20px',
-  }
+  //     marginTop: '20px', marginBottom: '20px',}
   return (
     <div>
       <img src={notfound} style={{width:'800px',height:'550px'}} alt="not found"/>
